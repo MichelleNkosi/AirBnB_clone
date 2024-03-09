894b6f85c0f0eb9a7a0c9cff568b1dd4bde2f3e3
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-09 00:27:14 +0000

    Task7: show and create methodes

--- console.py
+++ console.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Module console.py: controles the interaction with the models"""
 import cmd
-
+from models.base_model import BaseModel
+from models.__init__ import storage
 
 class HBNBCommand(cmd.Cmd):
     """Class contains all custom CMD commands"""
@@ -13,17 +14,51 @@ class HBNBCommand(cmd.Cmd):
         return True    
     do_EOF = do_quit
 
-    def do_create(self):
-        """Create new BaseModel Obj"""
-        pass
+    def do_create(self, args):
+        """Create new BaseModel Obj
+        Usage: create BaseModel"""
+        if args == "":
+            print("** class name missing **")
+        elif args == "BaseModel":            
+            new_obj = BaseModel()
+            new_obj.save()
+            print(new_obj.id)
+        else:
+            print("** class doesn't exist **")
 
-    def do_show(self):
+    def do_show(self, args):
         """Print string representation of instance based on class name and ID"""
-        pass
+        args_list = args.split()
+        if len(args_list) < 1:
+            print("** class name missing **")            
+        elif args_list[0] == "BaseModel":
+            if len(args_list) < 2:
+                print("** instance id missing **")
+            else:
+                all_objs = storage.all()
+                class_id = args_list[0] + "." + args_list[1]
+                if class_id in all_objs.keys():
+                    print(all_objs[class_id])
+                else:
+                    print("** no instance found **")
+        else:
+            print("** class doesn't exist **")            
 
-    def do_destroy(self):
-        """Deletes instance based on class name and id"""
-        pass
+    def do_destroy(self, args):
+        """Deletes instance base on class name and id"""
+        args_list = args.split()
+        if len(args_list) < 1:
+            print("** class name missing **")            
+        elif args_list[0] == "BaseModel":
+            if len(args_list) < 2:
+                print("** instance id missing **")
+            else:
+                all_objs = storage.all()
+                class_id = args_list[0] + "." + args_list[1]
+                if class_id in all_objs.keys():
+                    print(all_objs[class_id])
+                else:
+                    print("** no instance found **")
 
     def do_all(self):
         """Prints string representation of all instances.
